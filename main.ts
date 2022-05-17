7b6f5a4457baf01336a4332091d7e8ca8d9524cc
Philipp Strobl <ph.strobl@gmx.de>
2022-05-18 00:18:35 +0200

    Weitere Tests

--- main.ts
+++ main.ts
@@ -1,31 +1,31 @@
-
+let icon = 0
+let y = 0
+let x = 0
 basic.showIcon(1)
-
-basic.forever(() => {
-
+basic.forever(function () {
     input.onButtonPressed(Button.A, () => {
 
         for (let i = 20; i < 1000; i++) {
             music.playTone(i, 1)
         }
-        for (let i = 1000; i > 20; i--) {
-            music.playTone(i, 1)
+        for (let j = 1000; j > 20; j--) {
+            music.playTone(j, 1)
         }
 
     })
 
-    //basic.showString("Was geht", 100)
-    basic.pause(500)
-
-    for (let i = 0; i < 10; i++) {
-        for(let j = 0; j < 50; j++) {
-            let x = Math.floor(Math.random() * 5)
-            let y = Math.floor(Math.random() * 5)
-            led.plot(x,y)
-            basic.pause(50)
-            led.unplot(x,y)
+    input.onButtonPressed(Button.B, () => {
+        for (let index = 0; index < 10; index++) {
+            for (let index = 0; index < 50; index++) {
+                x = Math.floor(Math.random() * 5)
+                y = Math.floor(Math.random() * 5)
+                led.plot(x, y)
+                basic.pause(50)
+                led.unplot(x, y)
+            }
+            icon = Math.floor(Math.random() * 20)
+            basic.showIcon(icon)
         }
-        let icon = Math.floor(Math.random()*20)
-        basic.showIcon(icon)
-    }
+    })
+    
 })
